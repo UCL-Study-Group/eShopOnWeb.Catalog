276e20671524f91dfb635026374bab1d33c2554f
GameTweak <esbendam@live.dk>
2025-10-21 20:55:50 +0200

    Renamed til Dependency.cs

--- Catalog.Application/Dependency.cs
+++ Catalog.Application/Dependency.cs
@@ -4,11 +4,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Catalog.Application;
 
-public static class DependencyInjection
+public static class Dependency
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<CatalogService>();
+        services.AddScoped<ItemService>();
         services.AddScoped<BrandService>();
         services.AddScoped<TypeService>();
         
--- Catalog.Infrastructure/Dependency.cs
+++ Catalog.Infrastructure/Dependency.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Catalog.Infrastructure;
 
-public static class DependencyInjection
+public static class Dependency
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
